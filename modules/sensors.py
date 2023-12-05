1e015868abf9a03ae2a70a5c1a78f1db3594cacf
Suraj Kushwah <suraj@bugswriter.com>
2023-12-05 21:23:05 +0530

    adding sensors

--- modules/sensors.py
+++ modules/sensors.py
@@ -1,51 +1,32 @@
 import subprocess
-
-def parse_sensors_output(output):
-    sensor_data = {}
-    current_adapter = None
-    lines = output.split('\n')
-    for line in lines:
-        line = line.strip()
-        if line.startswith("Adapter:"):
-            current_adapter = line.split(":")[1].strip()
-            sensor_data[current_adapter] = {}
-        elif current_adapter and ':' in line:
-            key_value = line.split(":")
-            if len(key_value) >= 2:
-                key, value = key_value[0].strip(), ':'.join(key_value[1:]).strip()
-                sensor_data[current_adapter][key] = value
-    return sensor_data
-
-def get_sensor_info():
-    try:
-        output = subprocess.check_output(["sensors"], universal_newlines=True, stderr=subprocess.STDOUT)
-        return parse_sensors_output(output)
-    except (subprocess.CalledProcessError, FileNotFoundError) as e:
-        return {"Error": str(e)}
-
-sensor_info = get_sensor_info()
-print(sensor_info)
-
-import subprocess
 import re
 
 def parse_sensors_output(output):
     sensors_data = {}
+
     current_adapter = None
+    current_core = None
 
     for line in output.split('\n'):
         if line.strip():
             if 'Adapter' in line:
                 current_adapter = re.search(r'Adapter: (.+)', line).group(1)
-                sensors_data[current_adapter] = {}
-            elif 'temp' in line or 'Sensor' in line:
+            elif 'temp' in line or 'Sensor' in line or 'Core' in line:
                 parts = re.split(r'\s*:\s*', line)
                 
                 # Check if parts has the expected number of elements
                 if len(parts) == 2:
                     key = parts[0]
-                    value = parts[1]
-                    sensors_data[current_adapter][key] = value
+                    value = re.search(r'([+-]?\d+\.\d+)', parts[1])
+
+                    if value:
+                        if 'Core' in key:
+                            current_core = re.search(r'Core (\d+)', key).group(1)
+                            full_key = f"{current_adapter}_Core{current_core}_{key.replace(' ', '_')}"
+                        else:
+                            full_key = f"{current_adapter}_{key.replace(' ', '_')}"
+                        
+                        sensors_data[full_key] = float(value.group(1))
 
     return sensors_data
 
@@ -61,7 +42,6 @@ if __name__ == "__main__":
     sensors_data = get_sensors_data()
 
     if sensors_data:
-        print("Parsed Sensors Data:")
         print(sensors_data)
     else:
         print("Failed to retrieve sensors data.")
