dd479c1ad81e444d3c3e6867f3fe85ca9c22ff56
Suraj Kushwah <suraj@bugswriter.com>
2023-12-06 16:08:51 +0530

    new disk script

--- modules/disk.py
+++ modules/disk.py
@@ -1,50 +1,48 @@
 import os
 import socket
 import subprocess
-import re
+import json
 
 def get_smartctl_data():
-	disk = os.getenv("DISK_PATH")
+    disk_path = os.getenv("DISK_PATH")
 
-	if disk is None:
-		print("DISK_PATH environment variable is not set.")
-		return None
+    if disk_path is None:
+        print("DISK_PATH environment variable is not set.")
+        return None
 
-	command_output = subprocess.check_output(['smartctl', '-a', disk]).decode('utf-8')
+    command_output = subprocess.check_output(['smartctl', '-j', '-a', disk_path]).decode('utf-8')
 
+    # Parse JSON output
+    smart_data = json.loads(command_output)
 
-	# Extract relevant values from the output using regular expressions
-	hostname = socket.gethostname()
-	temperature = re.search(r'Temperature:\s+(\d+)\sCelsius', command_output).group(1)
-	available_spare = re.search(r'Available Spare:\s+(\d+)%', command_output).group(1)
-	percentage_used = re.search(r'Percentage Used:\s+(\d+)%', command_output).group(1)
-	data_units_read = re.search(r'Data Units Read:\s+(\d+)', command_output).group(1)
-	data_units_written = re.search(r'Data Units Written:\s+(\d+)', command_output).group(1)
-	host_read_commands = re.search(r'Host Read Commands:\s+(\d+)', command_output).group(1)
-	host_write_commands = re.search(r'Host Write Commands:\s+(\d+)', command_output).group(1)
-	power_cycles = re.search(r'Power Cycles:\s+(\d+)', command_output).group(1)
-	power_on_hours = re.search(r'Power On Hours:\s+(\d+)', command_output).group(1)
-	unsafe_shutdowns = re.search(r'Unsafe Shutdowns:\s+(\d+)', command_output).group(1)
-	media_and_data_integrity_errors = re.search(r'Media and Data Integrity Errors:\s+(\d+)', command_output).group(1)
+    # Extract relevant values from the JSON data
+    hostname = socket.gethostname()
+    temperature = smart_data.get('temperature', {}).get('current', 0)
+    available_spare = smart_data.get('nvme_smart_health_information_log', {}).get('available_spare', 0)
+    percentage_used = smart_data.get('nvme_smart_health_information_log', {}).get('percentage_used', 0)
+    data_units_read = smart_data.get('nvme_smart_health_information_log', {}).get('data_units_read', 0)
+    data_units_written = smart_data.get('nvme_smart_health_information_log', {}).get('data_units_written', 0)
+    host_read_commands = smart_data.get('nvme_smart_health_information_log', {}).get('host_reads', 0)
+    host_write_commands = smart_data.get('nvme_smart_health_information_log', {}).get('host_writes', 0)
+    power_cycles = smart_data.get('nvme_smart_health_information_log', {}).get('power_cycles', 0)
+    power_on_hours = smart_data.get('nvme_smart_health_information_log', {}).get('power_on_hours', 0)
+    unsafe_shutdowns = smart_data.get('nvme_smart_health_information_log', {}).get('unsafe_shutdowns', 0)
+    media_and_data_integrity_errors = smart_data.get('nvme_smart_health_information_log', {}).get('media_errors', 0)
 
-	return {
-		"host": hostname,
-		"temperature": int(temperature),
-		"available_spare": int(available_spare),
-		"percentage_used": int(percentage_used),
-		"data_units_read": int(data_units_read),
-		"data_units_written": int(data_units_written),
-		"host_read_commands": int(host_read_commands),
-		"host_write_commands": int(host_write_commands),
-		"power_cycles": int(power_cycles),
-		"power_on_hours": int(power_on_hours),
-		"unsafe_shutdowns": int(unsafe_shutdowns),
-		"media_and_data_integrity_errors": int(media_and_data_integrity_errors)
-	}
+    return {
+        "host": hostname,
+        "temperature": int(temperature),
+        "available_spare": int(available_spare),
+        "wearout": int(percentage_used),
+        "data_units_read": int(data_units_read),
+        "data_units_written": int(data_units_written),
+        "host_read_commands": int(host_read_commands),
+        "host_write_commands": int(host_write_commands),
+        "power_cycles": int(power_cycles),
+        "power_on_hours": int(power_on_hours),
+        "unsafe_shutdowns": int(unsafe_shutdowns),
+        "media_and_data_integrity_errors": int(media_and_data_integrity_errors)
+    }
 
-
-def collect_data():
-	return get_smartctl_data()
-
-if __name__=="__main__":
-	print(collect_data())
+result = get_smartctl_data()
+print(result)
